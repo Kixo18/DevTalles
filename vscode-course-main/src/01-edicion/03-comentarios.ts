55e2ec1690c2dcccf3016870488904504af7b6c1
Kixo18 <gombenja51@gmail.com>
2025-10-01 15:59:49 -0300

    Add files via upload

---
+++ vscode-course-main/src/01-edicion/03-comentarios.ts
@@ -0,0 +1,21 @@
+//*
+//*    Objetivo:
+//*        Comentar un bloque de código
+//*
+//*    Tips:
+//*        ⌘ /
+//*        
+//*        Ctrl + \
+//*
+
+const a = 30;
+const b = 40;
+const c = { a, b};
+
+
+console.log( a, b, c);
+
+// Resultado final
+// Sólo dejar un bloque comentado
+
+
